9415b4ffbdc82fc57547e8fa70042612f6fb4923
holly <holly@holly-laptop.(none)>
2013-08-10 07:39:29 +0800

    akk

---
+++ include/test.h
@@ -0,0 +1,20 @@
+/*===========================================
+*   Copyright (C) 2013 All rights reserved.
+*   
+*   company      : xiaomi
+*   author       : holly
+*   email        : holly@xiaomi.com
+*   date         ：2013-08-10 07:39:17
+*   description  ：
+*
+=============================================*/
+#ifndef _TEST_H_
+#define _TEST_H_
+
+
+kkk
+
+
+
+
+#endif //_TEST_H
